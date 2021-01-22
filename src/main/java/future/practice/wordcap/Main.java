7eb22b46ed678088703934446905ec2af0a6db1d
Naufal Prima Yoriko <primayoriko@gmail.com>
2021-01-22 16:42:24 +0700

    build: add dependecy for maven with String utils + fixing function

--- src/main/java/future/practice/wordcap/Main.java
+++ src/main/java/future/practice/wordcap/Main.java
@@ -4,19 +4,10 @@ import java.util.*;
 import future.practice.wordcap.features.WordCapitalize;
 
 public class Main {
-    public static void Main(){
-        Scanner sc= new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
 
-        ArrayList<String> output = WordCapitalize.capitalizeOne(input);
-        System.out.print("[");
-        for(int i = 0; i < output.size(); i++){
-            System.out.printf("\"%s\"", output.get(i));
-            if(i != output.size() - 1){
-                System.out.print(", ");
-            }
-        }
-        System.out.println("]");
-
+        WordCapitalize.printCapOne(input);
     }
 }
--- src/main/java/future/practice/wordcap/features/WordCapitalize.java
+++ src/main/java/future/practice/wordcap/features/WordCapitalize.java
@@ -1,12 +1,28 @@
 package future.practice.wordcap.features;
 
 import java.util.ArrayList;
-//import StringUtils.capitalize;
+//import StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class WordCapitalize {
-    public static ArrayList<String> capitalizeOne(String word){
-        ArrayList res = new ArrayList<String>();
+    public static String[] capitalizeOne(String word){
+        String[] res = StringUtils.split(word, " ");
+        for(int i = 0; i < res.length; i++){
+            res[i] = StringUtils.capitalize(res[i]);
+        }
 
         return res;
+    }
+
+    public static void printCapOne(String word){
+        String[] arr = capitalizeOne(word);
+        System.out.print("[");
+        for(int i = 0; i < arr.length; i++){
+            System.out.printf("\"%s\"", arr[i]);
+            if(i != arr.length - 1){
+                System.out.print(", ");
+            }
+        }
+        System.out.println("]");
     }
 }
